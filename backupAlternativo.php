bc746c277f9207e7847bdcf6ecb40cd207d624bf
nicolasdulio <nico_n44@hotmail.com>
2016-11-30 10:45:16 -0300

    Nueva forma de backupear
    Exporto solo datos con un nuevo archivo de backups. Para poder
    importarlo en una base solo cargada con estructura

--- backupAlternativo.php
+++ backupAlternativo.php
@@ -1,77 +1,30 @@
 <?php
 include "conexion.php";
 include "funciones.php";
-backup_tables($db_host,$db_usuario,$db_password,$db_nombre);
+//Includes class
+include('FKMySQLDump.php');
 
+//Connects to mysql server
+$connessione = @mysql_connect($db_host,$db_usuario,$db_password);
 
-/* backup the db OR just a table */
-//En la variable $talbes puedes agregar las tablas especificas separadas por comas:
-//profesor,estudiante,clase
-//O déjalo con el asterisco '*' para que se respalde toda la base de datos
-
-function backup_tables($host,$user,$pass,$name,$tables = '*')
-{
-   
-   $link = mysql_connect($host,$user,$pass);
-   mysql_select_db($name,$link);
-   
-   //get all of the tables
-   if($tables == '*')
-   {
-      $tables = array();
-      $result = mysql_query('SHOW TABLES');
-      while($row = mysql_fetch_row($result))
-      {
-         $tables[] = $row[0];
-      }
-   }
-   else
-   {
-      $tables = is_array($tables) ? $tables : explode(',',$tables);
-   }
-   
-   //cycle through
-   $return = "";
-   foreach($tables as $table)
-   {
-      $result = mysql_query('SELECT * FROM '.$table);
-      $num_fields = mysql_num_fields($result);
-      
-      $return .= 'DROP TABLE '.$table.';';
-      $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
-      $return.= "\n\n".$row2[1].";\n\n";
-      
-    for ($i = 0; $i < $num_fields; $i++)
-      {
-         while($row = mysql_fetch_row($result))
-         {
-            $return.= 'INSERT INTO '.$table.' VALUES(';
-            for($j=0; $j<$num_fields; $j++) 
-            {
-               $row[$j] = addslashes($row[$j]);
-               $row[$j] = ereg_replace("\n","\\n",$row[$j]);
-               if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return.= '""'; }
-               if ($j<($num_fields-1)) { $return.= ','; }
-            }
-            $return.= ");\n";
-         }
-      }
-      $return.="\n\n\n";
-   }
-   
-   //save file
-
-   //$nombreDelArchivo = 'db-backup-'.getFechaDelSistemaConGuiones().'.sql';
-   $nombreDelArchivo = 'db-backup.sql';
-   $ruta='backup/';
-   $handle = fopen($ruta.$nombreDelArchivo,'w+');
-   fwrite($handle,$return);
-   fclose($handle);
+//Set encoding
+mysql_query("SET CHARSET utf8");
+mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
 
 
 
-   echo"<script language='javascript'>window.location='logout.php'</script>";
-}
 
+//Creates a new instance of FKMySQLDump: it exports without compress and base-16 file
+$dumper = new FKMySQLDump($db_nombre,'backup/db-backup.sql',false,false);
+
+$params = array(
+   'skip_structure' => TRUE,
+   //'skip_data' => TRUE,
+);
+
+//Make dump
+$dumper->doFKDump($params);
+
+echo"<script language='javascript'>window.location='logout.php'</script>"
 
 ?>
